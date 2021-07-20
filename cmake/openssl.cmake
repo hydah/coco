465039ddbd6bb5086d55c567e635337e4ea96002
fisheryhe <fisheryhe@tencent.com>
2021-07-21 03:01:28 +0800

    update compiling

--- cmake/openssl.cmake
+++ cmake/openssl.cmake
@@ -1,16 +1,16 @@
 include(ExternalProject)
 
 # 源文件输出临时目录
-set(COCO_OPENSSL_ROOT ${CMAKE_SOURCE_DIR}/thirdparty/temp/src_temp/openssl-1.1.1g)
+set(COCO_OPENSSL_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/temp/src_temp/openssl-1.1.1g)
 
 # 编译后头文件和库输出目录
-set(BUILD_PREFIX_ROOT ${CMAKE_SOURCE_DIR}/thirdparty/temp/out_libs/openssl-1.1.1g)
+set(BUILD_PREFIX_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/temp/out_libs/openssl-1.1.1g)
 
 # 编译后头文件和库输出目录
 set(COCO_OPENSSL_LIB_DIR       ${BUILD_PREFIX_ROOT}/lib)
 set(COCO_OPENSSL_INCLUDE_DIR   ${BUILD_PREFIX_ROOT}/include)
 
-set(COCO_OPENSSL_SRC_URL       ${CMAKE_SOURCE_DIR}/thirdparty/openssl-1.1.1g.tar.gz)
+set(COCO_OPENSSL_SRC_URL       ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/openssl-1.1.1g.tar.gz)
 set(COCO_OPENSSL_CONFIGURE     cd ${COCO_OPENSSL_ROOT}/src/COCO_OPENSSL_PROJECT/ && ${COCO_OPENSSL_ROOT}/src/COCO_OPENSSL_PROJECT/config no-shared --prefix=${BUILD_PREFIX_ROOT})
 set(COCO_OPENSSL_MAKE          cd ${COCO_OPENSSL_ROOT}/src/COCO_OPENSSL_PROJECT/ && make)
 set(COCO_OPENSSL_INSTALL       cd ${COCO_OPENSSL_ROOT}/src/COCO_OPENSSL_PROJECT/ && make install)
--- cmake/st.cmake
+++ cmake/st.cmake
@@ -1,6 +1,9 @@
 add_subdirectory(${ST})
-set(ST_INCLUDE_DIR       ${CMAKE_CURRENT_BINARY_DIR}/thirdparty/st)
-set(ST_LIB_DIR   ${CMAKE_CURRENT_BINARY_DIR}/lib)
+
+exec_program(${CMAKE_COMMAND} ARGS -E copy ${CMAKE_CURRENT_BINARY_DIR}/thirdparty/st/st.h ${PROJECT_SOURCE_DIR}/lib/st/include/st.h)
+
+set(ST_INCLUDE_DIR       ${PROJECT_SOURCE_DIR}/lib/st/include)
+set(ST_LIB_DIR   ${PROJECT_SOURCE_DIR}/lib)
 
 # 添加头文件和库目录
 include_directories(${ST_INCLUDE_DIR})
